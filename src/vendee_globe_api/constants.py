b881bf96d9f54e82c74995916f6f6243766085cd
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-06-20 16:55:23 +0200

    put data prep steps in data_prep

--- src/vendee_globe_api/constants.py
+++ src/vendee_globe_api/constants.py
@@ -2,9 +2,4 @@ from pathlib import Path
 
 root_dir = Path(__file__).resolve().parent.parent.parent
 
-race_2024_path = root_dir / "data" / "race_2024.parquet"
-web_2024_path = root_dir / "data" / "web_2024.parquet"
-wiki_2024_path = root_dir / "data" / "wiki_2024.parquet"
-
-df_race_path = root_dir / "data" / "df_race.parquet"
-df_infos_path = root_dir / "data" / "df_infos.parquet"
+db_path = root_dir / "data" / "vendee_globe.sqlite"
