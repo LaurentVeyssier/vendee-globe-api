ff25c6d91e6d8aec2fe3db8f8c520e6963494089
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-06-24 18:15:05 +0200

    clean crud file

--- src/vendee_globe_api/db/crud.py
+++ src/vendee_globe_api/db/crud.py
@@ -5,10 +5,9 @@ from sqlalchemy.orm import Session
 from vendee_globe_api.db.models import BoatBase, RaceBase
 
 
-# Read (single trip by id)
-def get_full_race(trips_db: Session) -> List[RaceBase]:
-    return trips_db.query(RaceBase).all()
+def get_full_race(race_db: Session) -> List[RaceBase]:
+    return race_db.query(RaceBase).all()
 
 
-def get_boats(history_db: Session) -> List[BoatBase]:
-    return history_db.query(BoatBase).all()
+def get_boats(boat_db: Session) -> List[BoatBase]:
+    return boat_db.query(BoatBase).all()
