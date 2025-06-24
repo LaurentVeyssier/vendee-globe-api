94f07866a8e324d119f0cc0c462be8d2552bd0b5
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-06-24 18:50:22 +0200

    update crud with global db

--- src/vendee_globe_api/db/crud.py
+++ src/vendee_globe_api/db/crud.py
@@ -7,18 +7,18 @@ from sqlalchemy.orm import Session
 from vendee_globe_api.db.models import BoatBase, RaceBase
 
 
-def get_min_datetime(race_db: Session) -> datetime:
-    return race_db.query(func.min(RaceBase.date_time)).scalar()
+def get_min_datetime(db: Session) -> datetime:
+    return db.query(func.min(RaceBase.date_time)).scalar()
 
 
-def get_partial_race(race_db: Session, actual_time: datetime) -> List[RaceBase]:
+def get_partial_race(db: Session, actual_time: datetime) -> List[RaceBase]:
     return (
-        race_db.query(RaceBase)
+        db.query(RaceBase)
         .filter(RaceBase.date_time <= actual_time)
         .order_by(RaceBase.date_time)
         .all()
     )
 
 
-def get_boats(boat_db: Session) -> List[BoatBase]:
-    return boat_db.query(BoatBase).all()
+def get_boats(db: Session) -> List[BoatBase]:
+    return db.query(BoatBase).all()
