1d37fe7f95f255d55c4213c652cf0f7e284cebe3
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-06-24 18:27:36 +0200

    add partial race getter and min_datetime

--- src/vendee_globe_api/db/crud.py
+++ src/vendee_globe_api/db/crud.py
@@ -1,12 +1,23 @@
+from datetime import datetime
 from typing import List
 
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 from vendee_globe_api.db.models import BoatBase, RaceBase
 
 
-def get_full_race(race_db: Session) -> List[RaceBase]:
-    return race_db.query(RaceBase).all()
+def get_min_datetime(race_db: Session) -> datetime:
+    return race_db.query(func.min(RaceBase.date_time)).scalar()
+
+
+def get_partial_race(race_db: Session, actual_time: datetime) -> List[RaceBase]:
+    return (
+        race_db.query(RaceBase)
+        .filter(RaceBase.date_time <= actual_time)
+        .order_by(RaceBase.date_time)
+        .all()
+    )
 
 
 def get_boats(boat_db: Session) -> List[BoatBase]:
