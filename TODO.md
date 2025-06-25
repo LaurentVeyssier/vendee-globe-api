880103764d073b7690ecd9f6f286a87067f5c94d
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-06-25 10:43:44 +0200

    add blanks in crud

---
+++ TODO.md
@@ -0,0 +1,3 @@
+1. go to [crud.py](./src/vendee_globe_api/db/crud.py) and fill the blanks required to access the data
+2. go to [main.py](./src/vendee_globe_api/main.py) and fill the blanks required to create the API
+3. create a `config.yaml` file based on [config.yaml.example](./config.yaml.example) and run the API (find help in [README.md](./README.md))
--- src/vendee_globe_api/db/crud.py
+++ src/vendee_globe_api/db/crud.py
@@ -8,21 +8,22 @@ from vendee_globe_api.db.models import BoatBase, RaceBase
 
 
 def get_min_datetime(db: Session) -> datetime:
-    return db.query(func.min(RaceBase.date_time)).scalar()
+    # TODO: query the db to return the minimum datetime
+    pass
 
 
 def get_max_datetime(db: Session) -> datetime:
-    return db.query(func.max(RaceBase.date_time)).scalar()
+    r# TODO: query the db to return the maximum datetime
+    pass
 
 
 def get_partial_race(db: Session, actual_time: datetime) -> List[RaceBase]:
-    return (
-        db.query(RaceBase)
-        .filter(RaceBase.date_time <= actual_time)
-        .order_by(RaceBase.date_time)
-        .all()
-    )
+    # TODO: query the RaceBase db and do the following operations:
+    # 1. filter date_time < actual_time
+    # 2. order by date_time
+    pass
 
 
 def get_boats(db: Session) -> List[BoatBase]:
-    return db.query(BoatBase).all()
+    # TODO: query the db to return all boats information
+    pass
